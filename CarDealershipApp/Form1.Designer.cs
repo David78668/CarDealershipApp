4501c0400fb2a3d5330de11d0d822ae6267fca62
horak.david <horak.david@ssakhk.cz>
2022-12-20 16:56:35 +0100

    Scaffolded database and coded first two forms

--- CarDealershipApp/Form1.Designer.cs
+++ CarDealershipApp/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.formsPanel = new System.Windows.Forms.Panel();
+            this.formsSelectBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // formsPanel
+            // 
+            this.formsPanel.Location = new System.Drawing.Point(12, 57);
+            this.formsPanel.Name = "formsPanel";
+            this.formsPanel.Size = new System.Drawing.Size(816, 392);
+            this.formsPanel.TabIndex = 0;
+            // 
+            // formsSelectBox
+            // 
+            this.formsSelectBox.FormattingEnabled = true;
+            this.formsSelectBox.Location = new System.Drawing.Point(12, 12);
+            this.formsSelectBox.Name = "formsSelectBox";
+            this.formsSelectBox.Size = new System.Drawing.Size(204, 28);
+            this.formsSelectBox.TabIndex = 1;
+            this.formsSelectBox.SelectedIndexChanged += new System.EventHandler(this.formsSelectBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(840, 462);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formsSelectBox);
+            this.Controls.Add(this.formsPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Panel formsPanel;
+        private ComboBox formsSelectBox;
+        private Label label1;
     }
 }
--- CarDealershipApp/Form1.cs
+++ CarDealershipApp/Form1.cs
@@ -1,10 +1,42 @@
+using System.DirectoryServices.ActiveDirectory;
+
 namespace CarDealershipApp
 {
     public partial class Form1 : Form
     {
+        List<Form> FormList = new();
+        DealersForm dealersForm = new();
+
         public Form1()
         {
             InitializeComponent();
+            Init();
+        }
+
+        public void Init()
+        {
+            FormList.Add(dealersForm);
+
+            foreach (var form in FormList)
+            {
+                form.TopLevel = false;
+                form.AutoScroll = true;
+                form.FormBorderStyle = FormBorderStyle.None;
+                formsPanel.Controls.Add(form);
+                formsSelectBox.Items.Add(form.Text);
+            }
+        }
+
+        private void formsSelectBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           foreach(var form in FormList)
+            {
+                if (formsSelectBox.SelectedItem == form.Text)
+                {
+                    form.Show();
+                }
+            }
+
         }
     }
 }
